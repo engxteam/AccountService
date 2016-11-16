09b2cd8c3a8019b2ac5046684571995bdf4d041b
82dilip <82.dilip@gmail.com>
2016-11-16 20:40:42 +0530

    Added Account Server Code

---
+++ AccountServer/src/test/java/com/example/AccountServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+/*@SpringBootTest
+public class AccountServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
